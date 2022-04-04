41c45d706c18501485f4818adfb0ad8769f2d4ac
mihnea <umihnea52@gmail.com>
2022-04-04 12:14:53 +0300

    Add basic Redux configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import { DashboardPage, AddServicePage, UpdateServicePage } from './pages';
-import logo from './logo.svg';
+import {DashboardPage, AddServicePage, UpdateServicePage} from './pages';
 import './App.css';
 
 function App() {
   return (
-  <Router>
-    <div className="App">
-      <Routes>
-        <Route exact path="/" element={<DashboardPage />} />
-        <Route path="service">
-          <Route path="add" element={<AddServicePage />} />
-          <Route path="update/:serviceId" element={<UpdateServicePage />} />
-        </Route>
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<DashboardPage/>}/>
+          <Route path="service">
+            <Route path="add" element={<AddServicePage/>}/>
+            <Route path="update/:serviceId" element={<UpdateServicePage/>}/>
+          </Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
